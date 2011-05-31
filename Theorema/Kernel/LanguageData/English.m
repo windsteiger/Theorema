e44faacc8ee1743e4fd96df75ccb0ee129a06228
Marek Sacha <marek.sacha@benefio.cz>
2011-05-31 12:24:01 +0200

    loadArchive placed in the Theorema` context

--- Theorema/Kernel/LanguageData/English.m
+++ Theorema/Kernel/LanguageData/English.m
@@ -5,5 +5,6 @@ With[ {lang = "English"},
 	MessageName[$TheoremaDirectory, "usage", lang] = "The directory where the Theorema system is installed.";
 	MessageName[$TheoremaArchiveDirectory, "usage", lang] = "The directory where Theorema stores knowledge archives.";
 	MessageName[$TheoremaArchivePath, "usage", lang] = "The directory where Theorema searches for knowledge archives.";
+	MessageName[loadArchive, "usage", lang] = "loadArchive[ArchiveName`] loads the specified archive into the current session.";
 ]
 
