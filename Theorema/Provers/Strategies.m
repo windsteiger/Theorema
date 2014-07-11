8bdac09d3f3c2e96e23d0a3a1bff1963ad52ca3c
Wolfgang Windsteiger <Wolfgang.Windsteiger@risc.jku.at>
2014-07-11 13:18:18 +0200

    Licenes statements updated, minor docu change

--- Theorema/Provers/Strategies.m
+++ Theorema/Provers/Strategies.m
@@ -1,14 +1,14 @@
 (* Theorema 
     Copyright (C) 2010 The Theorema Group
 
-    This file is part of Theorema.2
+    This file is part of Theorema 2.0
     
-    Theorema.2 is free software: you can redistribute it and/or modify
+    Theorema 2.0 is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    Theorema.2 is distributed in the hope that it will be useful,
+    Theorema 2.0 is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
